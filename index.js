edc24eb17be23baba058ea026ef4b81778c5ac77
ibrahim senzia <ibrahimsenzia@gmail.com>
2024-03-26 20:09:26 +0300

    functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+
+// Function declaration for saturdayFun
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function expression for mondayWork
+  const mondayWork = function (activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Function expression for wrapAdjective
+
+  const wrapAdjective = (char) => {
+    if (typeof char !== 'string' || char.length !== 1) {
+      throw new Error('char must be a string with length 1');
+    }
+  
+    return (adj) => char + adj + char;
+  };
+  
+  const highlightWithAsterisks = wrapAdjective('*');
+  const highlightWithPipes = wrapAdjective('||');
+  
